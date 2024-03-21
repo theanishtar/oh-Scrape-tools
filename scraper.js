896de8cd141bd6b75d08f15aa11dbcae1ce7e1d8
Tran Huu Dang <89396036+theanishtar@users.noreply.github.com>
2024-03-21 11:39:49 +0700

    peft: update scraper.js

--- scraper.js
+++ scraper.js
@@ -12,23 +12,34 @@ async function scrapeData() {
   }
 }
 
-function parseHTML(html) {
-  // Viết mã để phân tích HTML và trích xuất dữ liệu cần thiết ở đây
-  // Đây là nơi bạn sẽ sử dụng các công cụ như DOMParser hoặc thư viện bên thứ ba như Cheerio
-  // Ví dụ:
-  const data = {
-    title: 'Sample Title',
-    content: 'Sample Content',
-  };
-  return data;
-}
+function htmlToMarkdown(html) {
+      // Chuyển đổi HTML thành Markdown ở đây
+      // Bạn có thể sử dụng một thư viện HTML-to-Markdown hoặc viết mã chuyển đổi tùy chỉnh
+      // Đây là một ví dụ đơn giản:
+      // (Lưu ý: Đây không phải là phương pháp hoàn chỉnh, mà chỉ là một ví dụ đơn giản)
+      let markdown = html
+        .replace(/<h1>/g, '# ')
+        .replace(/<h2>/g, '## ')
+        .replace(/<h3>/g, '### ')
+        .replace(/<h4>/g, '#### ')
+        .replace(/<h5>/g, '##### ')
+        .replace(/<h6>/g, '###### ')
+        .replace(/<\/h[1-6]>/g, '\n')
+        .replace(/<p>/g, '')
+        .replace(/<\/p>/g, '\n')
+        .replace(/<br>/g, '\n')
+        .replace(/<strong>/g, '**')
+        .replace(/<\/strong>/g, '**')
+        .replace(/<em>/g, '*')
+        .replace(/<\/em>/g, '*')
+        .replace(/<a[^>]* href="([^"]*)"[^>]*>(.*?)<\/a>/g, '[$2]($1)')
+        .replace(/<\/?[^>]+(>|$)/g, '');
+
+      return markdown;
+    }
 
 function generateMarkdown(data) {
-  const markdown = `
-# ${data.title}
-
-${data.content}
-  `;
+  const markdown = htmlToMarkdown(data);
   return markdown;
 }
 
